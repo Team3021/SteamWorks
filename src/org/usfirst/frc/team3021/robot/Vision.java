a3a334d06d3818e31b8a9c820b6e64d44c8705a5
Neightro <nathan@codiem.com>
2017-02-04 13:45:42 -0800

    Initial commit of VisionProcessor
    VisionProcessor currently provides an option in the dashboard which
    draws two white squares on the screen. More work on targeting is to
    come.

--- src/org/usfirst/frc/team3021/robot/Vision.java
+++ src/org/usfirst/frc/team3021/robot/Vision.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team3021.robot;
 
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
@@ -11,10 +10,11 @@ public class Vision {
 	Controller controller;
 	UsbCamera cam0;
 	UsbCamera cam1;
-	int curCam;
+	int curCamNum;
 	CameraServer server;
 	MjpegServer sink;
 	private boolean btn_down;
+	VisionProcessor visionProcessor;
 	
 	public Vision(){
 		server = CameraServer.getInstance();
@@ -22,7 +22,7 @@ public class Vision {
 		cam0 = new UsbCamera("Active USB Camera", 0);
 		cam1 = new UsbCamera("Active USB Camera", 1);
 		
-		curCam = 0;
+		curCamNum = 0;
 		
 		sink = new MjpegServer("Server 1", 1180);
 		
@@ -31,8 +31,14 @@ public class Vision {
 		server.addCamera(cam1);
 		
 		sink.setSource(cam0);
+		cam0.setFPS(20);
+		cam1.setFPS(20);
 		
 		System.out.println("Camera starting.");
+		
+		visionProcessor = new VisionProcessor(cam0);
+		visionProcessor.setDaemon(true);
+		visionProcessor.start();
 	}
 
 	public void setController(Controller controller) {
@@ -43,18 +49,22 @@ public class Vision {
 		if(controller.isSwitchingCamera() && !btn_down){
 			btn_down = true;
 			System.out.println("Switch cam\n");
-			if (curCam == 1) {
+			if (curCamNum == 1) {
 				sink.setSource(cam0);
-				curCam = 0;
+				curCamNum = 0;
+				visionProcessor.setCamera(cam0);
 			} 
-			else if (curCam == 0) {
+			else if (curCamNum == 0) {
 				sink.setSource(cam1);
-				curCam = 1;
+				curCamNum = 1;
+				visionProcessor.setCamera(cam1);
 			}
 		} 
 		else if (!controller.isSwitchingCamera()) {
 			btn_down = false;
 		}
 	}
+	
+	
 
 }
