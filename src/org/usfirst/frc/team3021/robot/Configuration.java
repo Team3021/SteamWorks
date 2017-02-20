ef51663c4513a250b562767ecaf72f667c9ce956
Trevor Hawrysh <trevor.hawrysh@thermofisher.com>
2017-02-19 23:52:54 -0800

    Add subsystems and commands to dashboard and create sub methods for display of choices

--- src/org/usfirst/frc/team3021/robot/Configuration.java
+++ src/org/usfirst/frc/team3021/robot/Configuration.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc.team3021.robot;
 
+import org.usfirst.frc.team3021.robot.commands.device.*;
+import org.usfirst.frc.team3021.robot.commands.driving.*;
+import org.usfirst.frc.team3021.robot.commands.test.*;
+
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -27,22 +32,69 @@ public class Configuration {
 	private SendableChooser<String> visionChooser = new SendableChooser<>();
 
 	public Configuration() {
+		
+		addAutonmousChoices();
+		
+		addControllerChoices();
+		
+		addVisionChoices();
+		
+		addSubsystemsToDashboard();
+		
+		addCommandsToDashboard();
+	}
+
+	private void addAutonmousChoices() {
 		autonomousChooser.addDefault("Autonmous : " + AUTONOMOUS_DEFALUT, AUTONOMOUS_DEFALUT);
 		SmartDashboard.putData("Auto choices", autonomousChooser);
-		
+	}
+
+	private void addControllerChoices() {
 		controllerChooser.addDefault(THRUSTMASTER, THRUSTMASTER );
 		controllerChooser.addObject(XBOX360, XBOX360);
 		SmartDashboard.putData("Select Controller", controllerChooser);
-		
-		visionChooser.addDefault(VISION_DISABLED, VISION_DISABLED);
-		visionChooser.addObject(VISION_ENABLED, VISION_ENABLED);
-		SmartDashboard.putData("Vision", visionChooser);
 		
 		SmartDashboard.putNumber(THRUSTMASTER_PORT, THRUSTMASTER_PORT_DEFAULT);
 		SmartDashboard.putNumber(XBOX360_PORT, XBOX360_PORT_DEFUALT);
 		SmartDashboard.putNumber(USBBUTTONS_PORT, USBBUTTONS_PORT_DEFAULT);
 	}
 
+	private void addVisionChoices() {
+		visionChooser.addDefault(VISION_DISABLED, VISION_DISABLED);
+		visionChooser.addObject(VISION_ENABLED, VISION_ENABLED);
+		SmartDashboard.putData("Vision", visionChooser);
+	}
+
+	private void addSubsystemsToDashboard() {
+		SmartDashboard.putData(Stanley.robotDrive);
+		SmartDashboard.putData(Stanley.collector);
+		SmartDashboard.putData(Stanley.launcher);
+		SmartDashboard.putData(Stanley.vision);
+	}
+	
+	private void addCommandsToDashboard() {
+		SmartDashboard.putData(Scheduler.getInstance());
+		
+		// Add test commands to dashboard
+		SmartDashboard.putData(new SubsystemTest());
+		SmartDashboard.putData(new DriveTest());
+		SmartDashboard.putData(new CollectorTest());
+		SmartDashboard.putData(new LauncherTest());
+		SmartDashboard.putData(new VisionTest());
+		
+		// Add drive commands to dashboard
+		SmartDashboard.putData(new StopDriving());
+		SmartDashboard.putData(new DriveToDistance(0.3, 3));
+		SmartDashboard.putData(new TurnToAngle(90.0));
+		SmartDashboard.putData(new TurnToCentralAngle(90.0));
+		
+		// Add device commands to dashboard
+		SmartDashboard.putData(new Agitate(5.0));
+		SmartDashboard.putData(new Index(5.0));
+		SmartDashboard.putData(new SpinWheel(5.0));
+		SmartDashboard.putData(new Collect(5.0));
+	}
+	
 	public String getAutonomousMode() {
 		return autonomousChooser.getSelected();
 	}
@@ -59,10 +111,10 @@ public class Configuration {
 		
 		int port = 0;
 
-		if (getJoystickMode() == THRUSTMASTER) {
+		if (getJoystickMode().equals(THRUSTMASTER)) {
 			port = (int) SmartDashboard.getNumber(THRUSTMASTER_PORT, THRUSTMASTER_PORT_DEFAULT);
 		}
-		else if (getJoystickMode() == XBOX360) {
+		else if (getJoystickMode().equals(XBOX360)) {
 			port = (int) SmartDashboard.getNumber(XBOX360_PORT, XBOX360_PORT_DEFUALT);
 		}
 		
