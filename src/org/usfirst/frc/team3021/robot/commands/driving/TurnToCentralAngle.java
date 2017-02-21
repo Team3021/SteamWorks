31632eed1ad748cc834eaefd1c33a6ee47687391
Neightro <nathan@codiem.com>
2017-02-20 16:43:14 -0800

    Add more values to dashboard

--- src/org/usfirst/frc/team3021/robot/commands/driving/TurnToCentralAngle.java
+++ src/org/usfirst/frc/team3021/robot/commands/driving/TurnToCentralAngle.java
@@ -18,6 +18,8 @@ public class TurnToCentralAngle extends DriveCommand {
 		super();
 		
 		desiredAngle = angle;
+		
+		SmartDashboard.putNumber("TurnToAngle : desired angle", desiredAngle);
 	}
 
 	@Override
@@ -38,10 +40,15 @@ public class TurnToCentralAngle extends DriveCommand {
 	@Override
 	protected boolean isFinished() {
 		
-		double angleDifference = Math.abs(Stanley.robotDrive.getGyroCentralAngle()) - Math.abs(desiredAngle);
+		double currentAbsAngle = Math.abs(Stanley.robotDrive.getGyroCentralAngle());
+		double currentAbsDesiredAngle = Math.abs(desiredAngle);
+		
+		double angleDifference = currentAbsAngle - currentAbsDesiredAngle;
 		angleDifference = Math.abs(angleDifference);
 		
 		SmartDashboard.putNumber("TurnToAngle : angle difference", angleDifference);
+		SmartDashboard.putNumber("TurnToAngle : desired angle", currentAbsDesiredAngle);
+		SmartDashboard.putNumber("TurnToAngle : current angle", currentAbsAngle);
 		
 		return (angleDifference <= GyroController.kToleranceDegrees);
 	}
