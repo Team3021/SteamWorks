60657ce5efe311caa35852db9529894dfe2c02b8
Neightro <nathan@codiem.com>
2017-02-22 20:27:01 -0800

    Change the variable name

--- src/org/usfirst/frc/team3021/robot/commands/driving/MoveForwardForDistance.java
+++ src/org/usfirst/frc/team3021/robot/commands/driving/MoveForwardForDistance.java
@@ -5,15 +5,15 @@ import org.usfirst.frc.team3021.robot.commands.DriveCommand;
 
 public class MoveForwardForDistance extends DriveCommand {
 	
-	protected double speed = 0;
-	protected double distance = 0;
+	protected double desiredSpeed = 0;
+	protected double desiredDistance = 0;
 	protected String direction = FORWARD;
 	
 	public MoveForwardForDistance(double speed, double distance) {
 		super();
 		
-		this.speed = speed;
-		this.distance = distance;
+		this.desiredSpeed = speed;
+		this.desiredDistance = distance;
 		
 		// set time out for safety
 		setTimeout(5);
@@ -21,14 +21,14 @@ public class MoveForwardForDistance extends DriveCommand {
 
 	@Override
 	protected void initialize() {
-		System.out.println("Start moving " + direction + " for distance : " + distance);
+		System.out.println("Start moving " + direction + " for distance : " + desiredDistance);
 		Stanley.robotDrive.resetGyro();
 		Stanley.robotDrive.resetEncoders();
 	}
 	
 	@Override
 	protected void execute() {
-		double moveValue = speed;
+		double moveValue = desiredSpeed;
 		
 		if (direction.equals(BACKWARD)) {
 			moveValue = -1.0 * moveValue;
@@ -39,13 +39,13 @@ public class MoveForwardForDistance extends DriveCommand {
 	
 	@Override
 	protected void end() {
-		System.out.println("End moving " + direction + " for distance : " + distance);
+		System.out.println("End moving " + direction + " for distance : " + desiredDistance);
 	}
 	
 	@Override
 	protected boolean isFinished() {
 		
-		double remaingDifference = Math.abs(distance) - Math.abs(Stanley.robotDrive.getDistance());
+		double remaingDifference = Math.abs(desiredDistance) - Math.abs(Stanley.robotDrive.getDistance());
 		
 		remaingDifference = Math.abs(remaingDifference);
 		
