e80ea6a449288e94bd7925890144dd23c5b8e66f
Neightro <nathan@codiem.com>
2017-03-04 20:57:27 -0800

    Tune the commands

--- src/org/usfirst/frc/team3021/robot/commands/auto/StartCenterToCenterGearDelivery.java
+++ src/org/usfirst/frc/team3021/robot/commands/auto/StartCenterToCenterGearDelivery.java
@@ -7,16 +7,18 @@ import org.usfirst.frc.team3021.robot.commands.driving.TurnLeftToAngle;
 import org.usfirst.frc.team3021.robot.commands.driving.TurnRightToAngle;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
 public class StartCenterToCenterGearDelivery extends CommandGroup {
 	public StartCenterToCenterGearDelivery() {
 		requires(Stanley.robotDrive);
 		
 		addSequential(new MoveForwardForDistance(0.4, 7));
+		addSequential(new TimedCommand(4));
 		addSequential(new MoveBackwardForDistance(0.3, 3.5));
-		addSequential(new TurnLeftToAngle(80));
+		addSequential(new TurnLeftToAngle(75));
 		addSequential(new MoveForwardForDistance(1.0, 7));
-		addSequential(new TurnRightToAngle(80));
+		addSequential(new TurnRightToAngle(75));
 		addSequential(new MoveForwardForDistance(1.0, 12));
 	}
 }
